b00656f6c4914e804befdc16c23f401159d628db
Asish-Ray <aray19069@gmail.com>
2024-09-26 23:17:31 +0530

    Project update 25/9

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -5,7 +5,10 @@ import { Label } from '../ui/label'
 import { Input } from '../ui/input'
 import { RadioGroup } from '../ui/radio-group'
 import { Button } from '../ui/button'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+import axios from 'axios'
+import { USER_API_END_POINT } from '@/utils/constant'
 
  const Login = () => {
   const [input,setInput]=useState({
@@ -15,14 +18,30 @@ import { Link } from 'react-router-dom'
     password: "",
     role: "",
     file: ""
-  })
+  });
+  const navigate = useNavigate;
   const changeEventHandler = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
 }
 const submitHandler = async (e) => {
   e.preventDefault();
-  
-  console.log(input);
+  try {
+    dispatch(setLoading(true));
+    const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        withCredentials: true,
+    });
+    if (res.data.success) {
+        dispatch(setUser(res.data.user));
+        navigate("/");
+        toast.success(res.data.message);
+    }
+} catch (error) {
+    console.log(error);
+    toast.error(error.response.data.message);
+}
 }
 
     return (
